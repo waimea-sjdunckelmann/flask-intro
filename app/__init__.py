75ca2b806b88bba1db37a9a050eb817b056c1970
waimea-sjdunckelmann <sjdunckelmann@waimea.school.nz>
2025-05-13 15:17:47 +1200

    test

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "<h1>Hello, World!<h1>"
+    return render_template('pages/home.jinja')
 
 
 @app.get("/random/")
@@ -20,4 +20,4 @@ def random():
 
 @app.get("/about/")
 def about():
-    return "<h1>Hello, World!<h1>"
+    return render_template('pages/about.jinja')
